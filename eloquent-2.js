95ebf2e5aa3f321b01724f67deec38380d597ac2
ushamacdonald <usha.macdonald@gmail.com>
2017-06-22 12:26:40 +1200

    Create eloquent-2.js

---
+++ eloquent-2.js
@@ -0,0 +1,23 @@
+// Looping a triangle
+var number = "#";
+while (number.length <= 7) {
+  console.log(number);
+  number = number + "#";
+}
+
+//FizzBuzz
+var number = 0;
+while (number <= 100) {
+number = number + 1;
+if (number % 3 === 0) {
+console.log("Fizz")
+}
+else if (number % 5 === 0) {
+console.log("Buzz")
+}
+else {
+console.log(number)
+}
+}
+
+//Chess Board
